759ef4bd0f3f2dd7322f186e179754193637a5f1
egorarhipov <arhipov_0406@mail.ru>
2024-09-24 10:09:21 +0300

    complete

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-
+        String firstName = "Ivan";
+        String lastName = "Ivanov";
+        String middleName = "Ivanovich";
+        System.out.println("Ф.И.О. сотрудника - " + lastName + " " + firstName + " " + middleName);
+        String fullName = lastName + " " + firstName + " " + middleName;
+        fullName = fullName.toUpperCase();
+        System.out.println("Данные Ф.И.О. сотрудника для заполнения отчета — " + fullName);
+        fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullName);
     }
 }
